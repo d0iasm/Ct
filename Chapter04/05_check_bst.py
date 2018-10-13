e5e2872ac1732ffc2ddd021bea2196ff6cda6b72
d0iasm <doiasami1219@gmail.com>
2018-10-13 14:51:49 +0900

    Added BST check by min max values

--- Chapter04/05_check_bst.py
+++ Chapter04/05_check_bst.py
@@ -7,6 +7,7 @@ class Node(object):
         self.left = left
         self.right = right
 
+
 last_printed = None
 def check_bst(node):
     global last_printed
@@ -21,6 +22,25 @@ def check_bst(node):
     last_printed = node.data
 
     if check_bst(node.right) is False:
+        return False
+
+    return True
+
+
+def check_bst_minmax(node):
+    return check_bst_minmax_helper(node, None, None)
+
+
+def check_bst_minmax_helper(n, min, max):
+    if n is None:
+        return True
+
+    if min is not None and n.data <= min or \
+            max is not None and n.data > max:
+        return False
+
+    if check_bst_minmax_helper(n.left, min, n.data) is False or \
+            check_bst_minmax_helper(n.right, n.data, max) is False:
         return False
 
     return True
@@ -83,6 +103,57 @@ class Test(unittest.TestCase):
         node_5.right = node_7
         root = node_2
         self.assertFalse(check_bst(root))
+
+    def test_true_balanced_bst_minmax(self):
+        node_1 = Node(1)
+        node_2 = Node(2)
+        node_3 = Node(3)
+        node_4 = Node(4)
+        node_5 = Node(5)
+        node_6 = Node(6)
+        node_7 = Node(7)
+        node_4.left = node_2
+        node_4.right = node_6
+        node_2.left = node_1
+        node_2.right = node_3
+        node_6.left = node_5
+        node_6.right = node_7
+        root = node_4
+        self.assertTrue(check_bst_minmax(root))
+
+    def test_true_unbalanced_bst_minmax(self):
+        node_1 = Node(1)
+        node_2 = Node(2)
+        node_3 = Node(3)
+        node_4 = Node(4)
+        node_5 = Node(5)
+        node_6 = Node(6)
+        node_7 = Node(7)
+        node_2.left = node_1
+        node_2.right = node_4
+        node_4.left = node_3
+        node_4.right = node_6
+        node_6.left = node_5
+        node_6.right = node_7
+        root = node_2
+        self.assertTrue(check_bst_minmax(root))
+
+    def test_false_unbalanced_bst_minmax(self):
+        node_1 = Node(1)
+        node_2 = Node(2)
+        node_3 = Node(3)
+        node_4 = Node(4)
+        node_5 = Node(5)
+        node_6 = Node(6)
+        node_7 = Node(7)
+        node_2.left = node_1
+        node_2.right = node_3
+        node_3.left = node_4
+        node_3.right = node_5
+        node_5.left = node_6
+        node_5.right = node_7
+        root = node_2
+        self.assertFalse(check_bst_minmax(root))
 
 
 if __name__ == '__main__':
