311b9c22bd111e22b9fe5e19e181f695b5437fda
d0iasm <doiasami1219@gmail.com>
2018-10-16 09:19:46 +0900

    Added a new solution for finding an ancestor

--- Chapter04/08_common_ancestor.py
+++ Chapter04/08_common_ancestor.py
@@ -39,6 +39,33 @@ def depth(n):
     return depth
 
 
+def covers(root, p):
+    if root is None:
+        return False
+    if root is p:
+        return True
+    return covers(root.left, p) or covers(root.right, p)
+
+
+def common_ancestor_without_parent(root, p, q):
+    if covers(root, p) is False or covers(root, q) is False:
+        return None
+    return ancestor_helper(root, p, q)
+
+
+def ancestor_helper(root, p, q):
+    if root is None or root is p or root is q:
+        return root
+
+    p_is_on_left = covers(root.left, p)
+    q_is_on_left = covers(root.left, q)
+    if p_is_on_left is not q_is_on_left:
+        return root
+
+    child_side = root.left if p_is_on_left else root.right
+    return ancestor_helper(child_side, p, q)
+
+
 class Test(unittest.TestCase):
     def test_top_common_ancestor(self):
         node_1 = Node(1)
@@ -141,6 +168,78 @@ class Test(unittest.TestCase):
         node_7.parent = node_6
 
         self.assertIsNone(common_ancestor(node_2, node_7))
+
+    def test_top_common_ancestor_wo_parent(self):
+        node_1 = Node(1)
+        node_2 = Node(2)
+        node_3 = Node(3)
+        node_4 = Node(4)
+        node_5 = Node(5)
+        node_6 = Node(6)
+        node_7 = Node(7)
+        node_4.left = node_2
+        node_4.right = node_6
+        node_2.left = node_1
+        node_2.right = node_3
+        node_6.left = node_5
+        node_6.right = node_7
+        root = node_4
+
+        self.assertEqual(4, common_ancestor_without_parent(root, node_2, node_7).data)
+
+    def test_middle_common_ancestor_wo_parent(self):
+        node_1 = Node(1)
+        node_2 = Node(2)
+        node_3 = Node(3)
+        node_4 = Node(4)
+        node_5 = Node(5)
+        node_6 = Node(6)
+        node_7 = Node(7)
+        node_4.left = node_2
+        node_4.right = node_6
+        node_2.left = node_1
+        node_2.right = node_3
+        node_6.left = node_5
+        node_6.right = node_7
+        root = node_4
+
+        self.assertEqual(6, common_ancestor_without_parent(root, node_5, node_7).data)
+
+    def test_same_common_ancestor_wo_parent(self):
+        node_1 = Node(1)
+        node_2 = Node(2)
+        node_3 = Node(3)
+        node_4 = Node(4)
+        node_5 = Node(5)
+        node_6 = Node(6)
+        node_7 = Node(7)
+        node_4.left = node_2
+        node_4.right = node_6
+        node_2.left = node_1
+        node_2.right = node_3
+        node_6.left = node_5
+        node_6.right = node_7
+        root = node_4
+
+        self.assertEqual(5, common_ancestor_without_parent(root, node_5, node_5).data)
+
+    def test_none_common_ancestor_wo_parent(self):
+        node_1 = Node(1)
+        node_2 = Node(2)
+        node_3 = Node(3)
+        node_4 = Node(4)
+        node_5 = Node(5)
+        node_6 = Node(6)
+        node_7 = Node(7)
+        node_4.left = node_2
+        node_2.left = node_1
+        node_2.right = node_3
+        root = node_4
+
+        node_6.left = node_5
+        node_6.right = node_7
+
+        self.assertIsNone(common_ancestor_without_parent(root, node_2, node_7))
 
 
 if __name__ == '__main__':
